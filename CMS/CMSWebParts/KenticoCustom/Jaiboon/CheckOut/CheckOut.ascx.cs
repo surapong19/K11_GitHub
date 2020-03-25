1a5359f31c45d2c0d9d395407e4cfafa7d99f2dc
surapong.phian19 <surapong.phian19@gmail.com>
2020-03-25 14:19:49 +0700

    ...

---
+++ CMS/CMSWebParts/KenticoCustom/Jaiboon/CheckOut/CheckOut.ascx.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Data;
+using System.Collections;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using CMS.Helpers;
+using CMS.PortalEngine.Web.UI;
+
+public partial class CMSWebParts_KenticoCustom_Jaiboon_CheckOut_CheckOut : CMSAbstractWebPart
+{
+    #region "Properties"
+
+    
+
+    #endregion
+
+
+    #region "Methods"
+
+    /// <summary>
+    /// Content loaded event handler.
+    /// </summary>
+    public override void OnContentLoaded()
+    {
+        base.OnContentLoaded();
+        SetupControl();
+    }
+
+
+    /// <summary>
+    /// Initializes the control properties.
+    /// </summary>
+    protected void SetupControl()
+    {
+        if (this.StopProcessing)
+        {
+            // Do not process
+        }
+        else
+        {
+            
+        }
+    }
+
+
+    /// <summary>
+    /// Reloads the control data.
+    /// </summary>
+    public override void ReloadData()
+    {
+        base.ReloadData();
+
+        SetupControl();
+    }
+
+    #endregion
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+
+    }
+}
